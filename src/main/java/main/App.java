42581b6747caf13224ec1256980012e4cbc654fc
KatriinaTee <katriina.tikkala@student.lut.fi>
2024-06-10 20:20:31 +0300

    Viikko 3

---
+++ src/main/java/main/App.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Hello world!
+ *
+ 1.  git add .
+   2.  git commit -m "VIESTI_TÄHÄN"
+   3.  git push origin main/* */
+   
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
